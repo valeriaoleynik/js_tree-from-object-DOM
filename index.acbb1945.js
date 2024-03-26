73e09892fa269493a163fe39cd026ff69ed76fd4
Valeria Oleynik <oleynik.valeria27@gmail.com>
2024-03-26 18:13:27 +0200

    make build

---
+++ index.acbb1945.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.append(r),t){var c=document.createElement("li");c.textContent=a,r.append(c),Object.keys(t[a]).length&&e(c,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.acbb1945.js.map
